bc0879d0883ed9e721db979b850986fe21f0a032
Amit Kumar <ext-amit.754.kumar@elisa.fi>
2026-04-10 17:36:33 +0530

    card name chnaged

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,77 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold text-center mt-4 text-green-500">My Profile</h1>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
- <Card imgSrc={"girl.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-   <Card imgSrc={"vicky.jpeg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-     <Card imgSrc={"img.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-            <Card imgSrc={"vicky.jpeg"} name={"Amit Kumar"} title={"Software Engineer"}/>
+      <h1 className="text-3xl font-bold text-center mt-4 text-green-500">
+        My Profile
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+        <Card
+          imgSrc={"girl.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"vicky.jpeg"}
+          name={"Vicky RAZ"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"img.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"vicky.jpeg"}
+          name={"Vicky RAZ"}
+          title={"Software Engineer"}
+        />
 
-       <Card imgSrc={"girl.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-     <Card imgSrc={"img.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-       <Card imgSrc={"vicky.jpeg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-       <Card imgSrc={"girl.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-     <Card imgSrc={"vicky.jpeg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-       <Card imgSrc={"girl.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-     <Card imgSrc={"img.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-       <Card imgSrc={"girl.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-     <Card imgSrc={"img.jpg"} name={"Amit Kumar"} title={"Software Engineer"}/>
-    </div>
- 
-
+        <Card
+          imgSrc={"girl.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"img.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"vicky.jpeg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"girl.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"vicky.jpeg"}
+          name={"Vicky RAZ"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"girl.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"img.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"girl.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+        <Card
+          imgSrc={"img.jpg"}
+          name={"Amit Kumar"}
+          title={"Software Engineer"}
+        />
+      </div>
     </>
   );
 }
